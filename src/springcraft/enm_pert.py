Fix covariance perturbation

src/springcraft/enm_pert.py
@@ -110,11 +110,32 @@ class ENMPert(ENM):
         if delta.size == 1:
             delta = np.repeat(delta, atom_i.size)
 
-        idx_i, idx_j, delta = self._modify_contact_values(atom_i, atom_j, delta)
+        if np.issubdtype(delta.dtype, np.bool):
+            disp = struc.displacement(self._coord[atom_i], self._coord[atom_j])
+            sq_dist = (disp * disp).sum(axis=1)
+
+            mask_on = delta
+            if self._ff.cutoff_distance is not None:
+                mask_on &= sq_dist < self._ff.cutoff_distance**2
+            mask_off = ~delta
+
+            force_constants = np.zeros(delta.size)
+            force_constants[mask_on] = self._ff.force_constant(
+                atom_i[mask_on], atom_j[mask_on], sq_dist[mask_on]
+            )
+
+            delta = np.select(
+                [mask_on, mask_off],
+                [
+                    -(force_constants + self._interactions[atom_i, atom_j]),  # pyright: ignore[reportOptionalSubscript]
+                    -self._interactions[atom_i, atom_j],  # pyright: ignore[reportOptionalSubscript]
+                ],
+                default=0,
+            )
 
         non_zero_mask = abs(delta) > 1e-8
         self._modify_contact_pair(
-            idx_i[non_zero_mask], idx_j[non_zero_mask], delta[non_zero_mask]
+            atom_i[non_zero_mask], atom_j[non_zero_mask], delta[non_zero_mask]
         )
 
     def modify_atom(self, atom_i, new_atom, skip_checks=False):
@@ -179,40 +200,14 @@ class ENMPert(ENM):
             np.repeat(atom_i, length - 1), atom_j, delta, skip_checks=True
         )
 
-    @abstractmethod
-    def _modify_contact_values(
-        self, atom_i: np.ndarray, atom_j: np.ndarray, delta: np.ndarray
-    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-        """
-        Helper method to calculate the changes that need to be made to
-        the `interaction` matrix when changing the strength of a contact
-        between atoms.
-
-        As this is a private function, no input checks are performed.
-
-        Parameters
-        ----------
-        atom_i : ndarray, shape=(k,), dtype=int
-            First atom index
-        atom_j : ndarray, shape=(k,), dtype=int
-            Second atom index with ``atom_i[idx] != atom_j[idx]``
-        delta : ndarray, shape=(k,), dtype=(float or bool)
-            The amount by which the interaction strength between
-            atom i and j gets changed in the `interaction` matrix.
-            True sets to initial value, False to 0. Must not be 0.
-
-        Returns
-        -------
-        idx_i : ndarray, shape=(k * dof,), dtype=int
-            First interaction index
-        idx_j : ndarray, shape=(k * dof,), dtype=int
-            Second interaction index
-        delta : ndarray, shape=(k * dof,), dtype=float
-            The amount to change.
-        """
-        pass  # pragma: no cover
-
-    def _modify_contact_pair(self, atom_i, atom_j, deltas, tem=None, tem_factors=K_B):
+    def _modify_contact_pair(
+        self,
+        atom_i: np.ndarray,
+        atom_j: np.ndarray,
+        deltas: np.ndarray,
+        tem: float | None = None,
+        tem_factors=K_B,
+    ):
         """
         Modifies the interaction strengths between the atoms i and j
         in the `interaction` matrix. Requires for the `interaction` matrix
@@ -247,39 +242,50 @@ class ENMPert(ENM):
         """
         for i, j, delta in np.nditer([atom_i, atom_j, deltas]):
             if self._covariance is not None:
-                x = self._covariance[i, :] - self._covariance[j, :]
-                beta = 1 + delta * (x[j] - x[i])
-                print("beta: ", beta)
+                x, eta = self._modify_contact_pair_vector(i, j)  # pyright: ignore[reportArgumentType]
+                beta = 1 + delta * eta
 
                 if np.abs(beta) < 1e-10:  # TODO use relative instead of absolute diff?
-                    self._modify_contact_pair_rank_decrease(x)
+                    self._modify_contact_pair_covariance_rank_decrease(x)
                 elif np.abs(self._covariance[i, j]) < 1e-10:  # TODO mathematical proof
-                    self._modify_contact_pair_rank_increase(i, j, delta, x, beta)
+                    self._modify_contact_pair_covariance_rank_increase(
+                        i, j, delta, x, beta
+                    )
                 else:
                     # default case
-                    self._covariance += np.outer(x * delta / beta, x)
+                    self._modify_contact_pair_covariance(x, delta, beta)  # pyright: ignore[reportArgumentType]
 
-            self._interactions[i, j] += delta  # pyright: ignore[reportOptionalSubscript]
-            self._interactions[j, i] += delta  # pyright: ignore[reportOptionalSubscript]
-            self._interactions[i, i] -= delta  # pyright: ignore[reportOptionalSubscript]
-            self._interactions[j, j] -= delta  # pyright: ignore[reportOptionalSubscript]
-
-            print(
-                "maxdiff: ",
-                np.max(
-                    np.abs(
-                        self._interactions
-                        - self._interactions @ self._covariance @ self._interactions
-                    )
-                ),
-            )
-
-            # TODO can we make it faster, if we only compute the upper triangle?
+            self._modify_contact_pair_interaction(i, j, delta)  # pyright: ignore[reportArgumentType]
 
         self._eig_values = None
         self._eig_vectors = None
 
-    def _modify_contact_pair_rank_decrease(self, x):
+    @abstractmethod
+    def _modify_contact_pair_vector(
+        self, atom_i: int, atom_j: int
+    ) -> tuple[np.ndarray, float]:
+        r"""
+        If the one-rank update to the interaction matrix A is described by
+
+        .. math::
+
+            A_\text{mod} = A - \delta \cdot \vec{u} \vec{u}^T
+
+        where :math:'\vec{u} is a vector than this function calculates
+
+        .. math::
+
+            x = A^{-1} \cdot \vec{u} = \vec{u}^T \cdot A^{-1} \\
+            \varepsilon = - \vec{u}^T \cdot A^{-1} \cdot \vec{u}
+
+        where :math;'()^{-1}' describes the pseudo-inverse
+        """
+        pass  # pragma: no cover
+
+    def _modify_contact_pair_covariance(self, x: np.ndarray, delta: float, beta: float):
+        self._covariance += np.outer(x * delta / beta, x)  # pyright: ignore[reportOperatorIssue]
+
+    def _modify_contact_pair_covariance_rank_decrease(self, x):
         cov_mul_diff = np.matvec(self._covariance, x)  # pyright: ignore[reportArgumentType]
         x_norm_sq = np.inner(x, x)
 
@@ -289,7 +295,7 @@ class ENMPert(ENM):
 
         self._covariance += dd_mul_cov + dd_mul_cov.T + k_cov_h_mul_kh
 
-    def _modify_contact_pair_rank_increase(self, i, j, delta, x, beta):
+    def _modify_contact_pair_covariance_rank_increase(self, i, j, delta, x, beta):
         y = -np.matvec(self._interactions, x)  # pyright: ignore[reportArgumentType]
         y[i] += 1
         y[j] -= 1
@@ -299,3 +305,14 @@ class ENMPert(ENM):
         beta_y_y = np.outer(y * beta / (-delta * y_norm_sq * y_norm_sq), y)
 
         self._covariance += x_y + x_y.T + beta_y_y
+
+    @abstractmethod
+    def _modify_contact_pair_interaction(self, atom_i: int, atom_j: int, delta: float):
+        """
+        Performs a one-rank permutation to the `interaction` matrix.
+        The permutation describes a change in the force constance between
+        atoms `atom_i` and `atom_j` by `delta`.
+
+        The `interaction` matrix must exists.
+        """
+        pass

src/springcraft/gnm.py
@@ -188,30 +188,16 @@ class GNM(ENMPert):
         self._kirchhoff = None
 
     @override
-    def _modify_contact_values(
-        self, atom_i: np.ndarray, atom_j: np.ndarray, delta: np.ndarray
-    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-        if np.issubdtype(delta.dtype, np.bool):
-            disp = struc.displacement(self._coord[atom_i], self._coord[atom_j])
-            sq_dist = (disp * disp).sum(axis=1)
+    def _modify_contact_pair_vector(
+        self, atom_i: int, atom_j: int
+    ) -> tuple[np.ndarray, float]:
+        x = self._covariance[atom_i, :] - self._covariance[atom_j, :]  # pyright: ignore[reportOptionalSubscript]
+        eps = x[atom_j] - x[atom_i]
+        return x, eps
 
-            mask_on = delta
-            if self._ff.cutoff_distance is not None:
-                mask_on &= sq_dist < self._ff.cutoff_distance**2
-            mask_off = ~delta
-
-            force_constants = np.zeros(delta.size)
-            force_constants[mask_on] = self._ff.force_constant(
-                atom_i[mask_on], atom_j[mask_on], sq_dist[mask_on]
-            )
-
-            delta = np.select(
-                [mask_on, mask_off],
-                [
-                    -(force_constants + self._interactions[atom_i, atom_j]),  # pyright: ignore[reportOptionalSubscript]
-                    -self._interactions[atom_i, atom_j],  # pyright: ignore[reportOptionalSubscript]
-                ],
-                default=0,
-            )
-
-        return atom_i, atom_j, delta
+    @override
+    def _modify_contact_pair_interaction(self, atom_i: int, atom_j: int, delta: float):
+        self._kirchhoff[atom_i, atom_j] += delta  # pyright: ignore[reportOptionalSubscript]
+        self._kirchhoff[atom_j, atom_i] += delta  # pyright: ignore[reportOptionalSubscript]
+        self._kirchhoff[atom_i, atom_i] -= delta  # pyright: ignore[reportOptionalSubscript]
+        self._kirchhoff[atom_j, atom_j] -= delta  # pyright: ignore[reportOptionalSubscript]

tests/test_gnm.py
@@ -11,7 +11,7 @@ from biotite.structure import AtomArray
 
 import springcraft
 
-from .util import data_dir
+from .util import ModifiedForceField, data_dir
 
 
 def prepare_gnm(file_path, cutoff):
@@ -412,7 +412,7 @@ def test_fluctuation_dcc(file_path, cutoff):
     assert np.allclose(test_dcc_absolute, reference_dcc_absolute)
 
 
-@pytest.mark.parametrize("nb_of_changes", [1, 3, 20, 27])
+@pytest.mark.parametrize("nb_of_changes", [1, 3, 20, 27, 200])
 def test_modify_contact_pair(nb_of_changes):
     """
     Tests whether permutations to the `kirchhoff` matrix are
@@ -431,25 +431,21 @@ def test_modify_contact_pair(nb_of_changes):
     ca = atoms[(atoms.atom_name == "CA") & (atoms.element == "C")]
     ff = springcraft.InvariantForceField(7.9)
 
-    gnm = springcraft.GNM(ca, ff)
-    ref_kirchhoff = gnm.kirchhoff.copy()
-    gnm.covariance
-
     rng = np.random.default_rng(0)
-    atom_i, atom_j = np.zeros((2, nb_of_changes), dtype=int)
-    delta = (rng.random(nb_of_changes) + 0.5) * rng.choice((-1, 1), nb_of_changes)
-    for k in range(nb_of_changes):
-        atom_i[k], atom_j[k] = rng.choice(len(ca), size=2, replace=False)
+    atom_i = rng.integers(0, len(ca), size=nb_of_changes)
+    offsets = rng.integers(1, len(ca), size=nb_of_changes)
+    atom_j = (atom_i + offsets) % len(ca)
+    delta = rng.random(nb_of_changes) * 2 - 1
 
-        ref_kirchhoff[atom_i[k], atom_j[k]] += delta[k]
-        ref_kirchhoff[atom_j[k], atom_i[k]] += delta[k]
-        ref_kirchhoff[atom_i[k], atom_i[k]] -= delta[k]
-        ref_kirchhoff[atom_j[k], atom_j[k]] -= delta[k]
-
-    ref_gnm = springcraft.GNM(ca, ff)
-    ref_gnm.kirchhoff = ref_kirchhoff
+    ref_ff = ModifiedForceField(ff, len(ca), atom_i, atom_j, delta)
+    ref_gnm = springcraft.GNM(ca, ref_ff)
+    ref_kirchhoff = ref_gnm.kirchhoff
     ref_covariance = ref_gnm.covariance
 
+    gnm = springcraft.GNM(ca, ff)
+    gnm.kirchhoff
+    gnm.covariance
+    assert gnm._covariance is not None
     gnm._modify_contact_pair(atom_i, atom_j, delta)
     mod_kirchhoff = gnm.kirchhoff
     mod_covariance = gnm.covariance
@@ -478,33 +474,37 @@ def test_modify_contact(atom_i, atom_j, delta, expected_kirchhoff_change):
     ca = atoms[(atoms.atom_name == "CA") & (atoms.element == "C")]
     ff = springcraft.TabulatedForceField.d_enm(ca)
 
+    np.set_printoptions(precision=4, suppress=True, linewidth=300)
+
     gnm = springcraft.GNM(ca, ff)
-    ref_kirchhoff = gnm.kirchhoff
-    ref_kirchhoff[3, 7] -= 2.4
-    ref_kirchhoff[15, 18] += 11.7
-    ref_kirchhoff[8, 11] += 1e-9
-    ref_kirchhoff = ref_kirchhoff.copy()
+    print(gnm.kirchhoff)
+
+    idx_i = np.array([3, 15, 8])
+    idx_j = np.array([7, 18, 11])
+    val = np.array([-2.4, 11.7, 1e-9])
+    init_ff = ModifiedForceField(ff, len(ca), idx_i, idx_j, val)
+    init_gnm = springcraft.GNM(ca, init_ff)
+    init_kirchhoff = init_gnm.kirchhoff
+
+    gnm = springcraft.GNM(ca, ff)
+    gnm.kirchhoff = init_kirchhoff
     gnm.covariance
-
-    if type(atom_i) is list:
-        for i, j, d in zip(atom_i, atom_j, expected_kirchhoff_change):
-            ref_kirchhoff[i, j] += d
-            ref_kirchhoff[j, i] += d
-            ref_kirchhoff[i, i] -= d
-            ref_kirchhoff[j, j] -= d
-    else:
-        ref_kirchhoff[atom_i, atom_j] += expected_kirchhoff_change
-        ref_kirchhoff[atom_j, atom_i] += expected_kirchhoff_change
-        ref_kirchhoff[atom_i, atom_i] -= expected_kirchhoff_change
-        ref_kirchhoff[atom_j, atom_j] -= expected_kirchhoff_change
-
-    ref_gnm = springcraft.GNM(ca, ff)
-    ref_gnm.kirchhoff = ref_kirchhoff
-    ref_covariance = ref_gnm.covariance
-
+    assert gnm._covariance is not None
     gnm.modify_contact(atom_i, atom_j, delta)
     mod_kirchhoff = gnm.kirchhoff
     mod_covariance = gnm.covariance
+
+    if not hasattr(atom_i, "__iter__"):
+        atom_i = [atom_i]
+        atom_j = [atom_j]
+        expected_kirchhoff_change = [expected_kirchhoff_change]
+    idx_i = np.concatenate([idx_i, atom_i])
+    idx_j = np.concatenate([idx_j, atom_j])
+    val = np.concatenate([val, expected_kirchhoff_change])
+    ref_ff = ModifiedForceField(ff, len(ca), idx_i, idx_j, val)
+    ref_gnm = springcraft.GNM(ca, ref_ff)
+    ref_kirchhoff = ref_gnm.kirchhoff
+    ref_covariance = ref_gnm.covariance
 
     assert np.allclose(mod_kirchhoff, ref_kirchhoff)
     assert np.allclose(mod_covariance, ref_covariance)